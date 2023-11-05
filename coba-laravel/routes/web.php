77d54d35c2a2e69ca5a38ee322dbf15e04a2bb6b
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-05 13:35:20 +0700

    update day 1

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -18,7 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        "name" => "leonardo yemi",
+        "email" => "leonardoyemi@gmail.com",
+        "image" => "gambar1.png"
+    ]);
 });
 
 Route::get('/blog', function () {

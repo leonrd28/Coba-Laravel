e3bd8996493cf3f0ecba93e024e72e20b478d51e
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-06 15:59:47 +0700

    update day 1

--- coba-laravel/resources/views/about.blade.php
+++ coba-laravel/resources/views/about.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Beruanganeh Blog | About</title>
-</head>
-<body>
-      <h1>halaman About</h1>
+@extends('layouts.main')
 
-      <h3><?= $name; ?></h3>
-      <p><?= $email; ?></p>
-      <img src="img/<?= $image;?>" alt="<?= $name; ?>" width="200">
-</body>
-</html>
+
+
+
+@section('container')
+      <h1>halaman About</h1>
+      <h3>{{ $name }}</h3>
+      <p>{{  $email }}</p>
+  <img src="img/{{ $image }}" alt=" {{ $name }}" width="200">
+@endsection

a097332e9196e4c7b9930f1a21978d069564c003
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-08 09:18:12 +0700

    update learning day 3 laravel

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,61 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name' => 'leonardo yemi',
+            'email' => 'leonardoyemi@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'beruanganeh',
+            'email' => 'bberuanganeh@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'web programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'web design',
+            'slug' => 'web-design'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'judul pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'lorem ipsum pertama',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, fuga consectetur id laborum necessitatibus eveniet impedit, quibusdam sint voluptatibus magnam repellendus doloremque! Labore maxime tempore vero. Modi deserunt non animi labore, deleniti nulla magnam sapiente inventore, nemo porro hic voluptatibus consectetur assumenda libero neque praesentium sunt ex quos. Autem sapiente unde qui veritatis? Excepturi deserunt cumque suscipit dolorum pariatur. Accusantium velit neque nam, at necessitatibus eveniet iste earum placeat odit sapiente delectus, iure libero omnis, repudiandae id corporis dolorem illum dignissimos pariatur. Repellendus reprehenderit quaerat quas officiis repudiandae quidem aperiam corporis ea fugit, atque magnam quod ad repellat placeat provident, dolore perspiciatis molestiae! In adipisci distinctio quos ab quis, quisquam impedit eius ipsum natus tempora voluptatibus perspiciatis vero laudantium voluptate quam porro dolore inventore delectus optio ullam culpa quae. Corrupti ipsum omnis, harum saepe ipsam dolorem cumque possimus fugit sed tenetur suscipit vero reiciendis, minima provident recusandae nisi, impedit quia.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'judul Kedua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'lorem ipsum Kedua',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, fuga consectetur id laborum necessitatibus eveniet impedit, quibusdam sint voluptatibus magnam repellendus doloremque! Labore maxime tempore vero. Modi deserunt non animi labore, deleniti nulla magnam sapiente inventore, nemo porro hic voluptatibus consectetur assumenda libero neque praesentium sunt ex quos. Autem sapiente unde qui veritatis? Excepturi deserunt cumque suscipit dolorum pariatur. Accusantium velit neque nam, at necessitatibus eveniet iste earum placeat odit sapiente delectus, iure libero omnis, repudiandae dolore perspiciatis molestiae! In adipisci distinctio quos ab quis, quisquam impedit eius ipsum natus tempora voluptatibus perspiciatis vero laudantium voluptate quam porro dolore inventore delectus optio ullam culpa quae. Corrupti ipsum omnis, harum saepe ipsam dolorem cumque possimus fugit sed tenetur suscipit vero reiciendis, minima provident recusandae nisi, impedit quia.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'judul Ketiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'lorem ipsum Ketiga',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, fuga consectetur id laborum necessitatibus eveniet impedit, quibusdam sint voluptatibus magnam repellendus doloremque! Labore maxime tempore vero. Modi deserunt non animi labore, deleniti nulla magnam sapiente inventore, nemo porro hic voluptatibus consectetur assumenda libero neque praesentium sunt ex quos. Autem sapiente unde qui veritatis? Excepturi deserunt cumque suscipit dolorum pariatur. Accusantium velit neque nam, at necessitatibus eveniet iste earum placeat odit sapiente delectus, iure libero omnis, repudiandae dolore perspiciatis molestiae! In adipisci distinctio quos ab quis, quisquam impedit eius ipsum natus tempora voluptatibus perspiciatis vero laudantium voluptate quam porro dolore inventore delectus optio ullam culpa quae. Corrupti ipsum omnis, harum saepe ipsam dolorem cumque possimus fugit sed tenetur suscipit vero reiciendis, minima provident recusandae nisi, impedit quia.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'judul Keempat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'lorem ipsum Keempat',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, fuga consectetur id laborum necessitatibus eveniet impedit, quibusdam sint voluptatibus magnam repellendus doloremque! Labore maxime tempore vero. Modi deserunt non animi labore, deleniti nulla magnam sapiente inventore, nemo porro hic voluptatibus consectetur assumenda libero neque praesentium sunt ex quos. Autem sapiente unde qui veritatis? Excepturi deserunt cumque suscipit dolorum pariatur. Accusantium velit neque nam, at necessitatibus eveniet iste earum placeat odit sapiente delectus, iure libero omnis, repudiandae dolore perspiciatis molestiae! In adipisci distinctio quos ab quis, quisquam impedit eius ipsum natus tempora voluptatibus perspiciatis vero laudantium voluptate quam porro dolore inventore delectus optio ullam culpa quae. Corrupti ipsum omnis, harum saepe ipsam dolorem cumque possimus fugit sed tenetur suscipit vero reiciendis, minima provident recusandae nisi, impedit quia.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,11 +4,11 @@
 <article>
   <h1 class="mb-5">{{ $post->title }}</h1>
 
-  <p>by. leonardo yemi in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+  <p>by. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}</a></p>
 
   {!! $post->body !!}
 </article>
 
-<a href="/posts">Back To Posts</a>
+<a href="/posts" class="d-block mt-3">Back To Posts</a>
 
 @endsection

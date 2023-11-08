9c7b5799e6d2ac78f8b718ce86df0bfc12887a15
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-08 08:32:49 +0700

    factory tutorial finally done day 3 laravel

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
@@ -53,5 +55,13 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => 'User Post',
+        'posts' => $author->posts,
+
     ]);
 });

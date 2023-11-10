7fbf99dee794048de0d8f3992e9ce336f333d401
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-10 17:50:29 +0700

    learning laravel day 6 register & login UI

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
-use App\Models\User;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +49,8 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);

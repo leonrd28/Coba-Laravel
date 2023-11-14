716bfee0c33eca5c8d9e26fea6aacdbe96af6d07
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-14 14:19:10 +0700

    learning laravel day 8 create form posts

--- coba-laravel/app/Http/Controllers/DashboardPostController.php
+++ coba-laravel/app/Http/Controllers/DashboardPostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
 {
@@ -22,7 +24,9 @@ class DashboardPostController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.posts.create', [
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -30,7 +34,12 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
     }
 
     /**
@@ -65,5 +74,11 @@ class DashboardPostController extends Controller
     public function destroy(Post $post)
     {
         //
+    }
+
+    public function checkSlug(Request $request)
+    {
+        $slug = SlugService::createSlug(Post::class, 'slug', $request->title);
+        return response()->json(['slug' => $slug]);
     }
 }
--- coba-laravel/resources/views/dashboard/layouts/main.blade.php
+++ coba-laravel/resources/views/dashboard/layouts/main.blade.php
@@ -11,6 +11,17 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+    {{-- trix editor --}}
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+
+
+    <style>
+      trix-toolbar [data-trix-bottom-group="file-tools"] {
+        display:none;
+      }
+    </style>
   </head>
   <body>
     

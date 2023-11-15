4870e3c69b3744539cfe89c890c0e25a0ea73d86
leonardoyemi <leonardoyemi28@gmail.com>
2023-11-15 19:47:47 +0700

    learning laravel day 9 authentication done

--- coba-laravel/app/Providers/AppServiceProvider.php
+++ coba-laravel/app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Paginator::useBootstrapFour();
+
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin;
+        });
     }
 }
